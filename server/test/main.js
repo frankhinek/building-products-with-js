89a4d4b310d3701f81ceb234ea33a85ddb10c18a
Frank Hinek <frank.hinek@gmail.com>
2016-07-27 23:55:05 -0400

    Test that a 404 error is returned if a non-existant URL is requested

--- server/test/main.js
+++ server/test/main.js
@@ -19,3 +19,18 @@ test('GET /', (t) => {
       t.end();
     });
 });
+
+test('Return 404 on nonexistant URL', (t) => {
+  request(app)
+    .get('/GETShouldFailOnNonExistantURL')
+    .expect(404)
+    .expect('Content-Type', /text\/html/)
+    .end((err, res) => {
+      const expectedBody = 'Cannot GET /GETShouldFailOnNonExistantURL\n';
+      const actualBody = res.text;
+
+      t.error(err, 'No error');
+      t.equal(actualBody, expectedBody, 'Retrieve body');
+      t.end();
+    });
+});
