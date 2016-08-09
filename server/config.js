8e74e8f3a1fa55fdc5903106530a855b6c1d8d96
Frank Hinek <frank.hinek@gmail.com>
2016-08-09 15:29:08 -0400

    Add cookie and session handling to Express

--- server/config.js
+++ server/config.js
@@ -6,4 +6,5 @@ export const db = {
 
 export const auth = {
   passwordSalt: '*j"=tEVYhOecig0FBUnFjT7BC0MR(Yeo)0YZax2KIUdxvT3Xt{',
+  sessionSecret: '4l]}WQiWFYZkW;i|Nq{uhbLxFJZr4FfdhByiH8c|Jkv4s5u2Vn',
 };
--- server/src/app.js
+++ server/src/app.js
@@ -1,10 +1,14 @@
 // npm packages
 import express from 'express';
 import bodyParser from 'body-parser';
+import cookieParser from 'cookie-parser';
+import session from 'express-session';
+import passport from 'passport';
 import morgan from 'morgan';
 
 // our packages
 import { logger } from './util';
+import { auth as authConfig } from '../config';
 
 // init app
 const app = express();
@@ -15,6 +19,21 @@ app.use(morgan('combined', { stream: logger.stream }));
 // add body parsing
 app.use(bodyParser.json()); // for parsing application/json
 app.use(bodyParser.urlencoded({ extended: true })); // for parsing application/x-www-form-urlencoded
+
+// add cookie parsing
+app.use(cookieParser());
+
+// add session support
+app.use(session({
+  secret: authConfig.sessionSecret,
+  resave: false,
+  saveUninitialized: true,
+  cookie: { secure: true }, // only set to true if using HTTPS
+}));
+
+// add passport js
+app.use(passport.initializat());
+app.use(passport.session());
 
 // test method
 app.get('/', (req, res) => {
