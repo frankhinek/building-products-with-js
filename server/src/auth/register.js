178f0d6f8fd3a4d820be499d1807c9aed6cf051d
Frank Hinek <frank.hinek@gmail.com>
2016-08-09 23:05:32 -0400

    Refactored authentication registration

--- server/src/auth/register.js
+++ server/src/auth/register.js
@@ -7,13 +7,23 @@ export default (app) => {
     // get user input
     const { login, password, passwordRepeat } = req.body;
 
+    // verify the same password was entered and confirmed
     if (password !== passwordRepeat) {
       res.status(400).send({ error: 'Passwords do not match' });
       return;
     }
 
+    // hash password
     const hashedPassword = hash(password);
 
+    // check if login already exists
+    const users = await User.filter({ login }).run();
+    if (users.length > 0) {
+      res.status(403).send({ error: 'User already exists!' });
+      return;
+    }
+
+    // create new User and store in database
     const user = new User({
       login,
       password: hashedPassword,
