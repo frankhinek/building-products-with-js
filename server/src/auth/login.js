19e3f65fc78e77789c86d5044211c9a201ace735
Frank Hinek <frank.hinek@gmail.com>
2016-08-09 23:04:20 -0400

    Refactored authentication login and noted existing bug

--- server/src/auth/login.js
+++ server/src/auth/login.js
@@ -3,6 +3,12 @@ import passport from 'passport';
 
 export default (app) => {
   app.post('/api/login', passport.authenticate('local'), (req, res) => {
-    res.send(req.user);
+    if (req.user) {
+      res.send({ user: req.user });
+    } else {
+      // TODO: This code is never called and needs to be refactored
+      // bugref: https://github.com/yamalight/building-products-with-js/issues/7
+      res.status(401).send({ error: 'Error logging in!' });
+    }
   });
 };
